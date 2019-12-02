00857d199e78226773db974484a3b5948541c2c7
meiQinQ <1569677451@qq.com>
2019-12-02 23:53:28 +0800

    feat；首页

--- src/views/student/stdHome.vue
+++ src/views/student/stdHome.vue
@@ -1,13 +1,8 @@
 <template>
  <div class="stdhome">
     <navmenu />
-    <el-carousel indicator-position="outside">
-      <el-carousel-item v-for="url in urls" :key="url">
-        <el-image :src="url" fit="contain"></el-image>
-      </el-carousel-item>
-    </el-carousel>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>欢迎进入公费医疗自助报销系统</h1>
+    <el-button type="primary" round @click="next">开始录入报销凭证</el-button>
   </div>
 </template>
 
@@ -21,36 +16,14 @@ export default {
   components: {
     navmenu
   },
-  data() {
-    return {
-      urls: [
-          'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-          'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-          'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-          'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-          'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-          'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-          'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
-        ]
-    }
-  },
+  methods:{
+    next() {
+      this.$router.push('/uploadVoucher')
+    },
+  }
 }
 </script>
 
 <style>
-  .el-carousel__item el-image {
-    color: #475669;
-    opacity: 1;
-    height: 500px;
-    line-height:500px;
-    margin: 0;
-  }
   
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
 </style>
